7f13b2b7400283f7f17eb61f75b6001a55859ea4
goodchuck <y502100@naver.com>
2024-01-05 21:25:47 +0900

    design : 사이드바에 ant-design적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { MSWComponent } from './_component/MSWComponent'
 import AuthSession from './_component/AuthSession'
+import Sidebar from './_component/Sidebar'
+import styles from './layout.module.css'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,12 +21,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div></div>
+      <body className={`${inter.className}`}>
         <MSWComponent />
         <AuthSession>
+
           <h1>next.js의 여러 기능들을 테스트를 하는 곳이며 나중에 다시 재사용 하기위한 저장소</h1>
-          {children}
+          <div className={styles['bodyWrapper']}>
+            <Sidebar />
+            <AntdRegistry>{children}</AntdRegistry>
+          </div>
+
         </AuthSession>
 
       </body>
