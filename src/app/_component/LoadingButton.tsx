b1a236f541ff3b38a108b1cecf9e31a96929dd00
goodchuck <y502100@naver.com>
2024-02-19 20:38:27 +0900

    fix : zod no-server-action페이지 로직개선 1. formData 필수 선택에 따라 제출버튼 disabled처리 2. submit후 에러가 발생할 시 에러문구 나오게 수정

--- src/app/_component/LoadingButton.tsx
+++ src/app/_component/LoadingButton.tsx
@@ -1,26 +1,40 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PoweroffOutlined } from '@ant-design/icons';
 import { Button, Flex } from 'antd';
 
-export const LoadingButton = (props: any) => {
+type Props = {
+    times?: number;
+    onCompleted?: boolean;
+    [key: string]: any;
+}
+
+export const LoadingButton = ({ times, onCompleted, ...rests }: Props) => {
     const [loading, setLoading] = useState<boolean>();
-    let times
-    if (props.times) {
-        times = props.times;
-    }
-    else {
-        times = 2000;
-    }
     const enterLoading = () => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, times);
+        if (onCompleted !== undefined) {
+            setLoading(true);
+        }
+        else if (times) {
+            setLoading(true);
+            setTimeout(() => {
+                setLoading(false);
+            }, times);
+        }
+        else {
+            setLoading(true);
+        }
+
     }
 
+    useEffect(() => {
+        if (onCompleted !== undefined && onCompleted) {
+            setLoading(false);
+        }
+    }, [onCompleted])
+
     return (
-        <Button type="primary" loading={loading} onClick={() => enterLoading()} {...props} >
+        <Button type="primary" loading={loading} onClick={() => enterLoading()} {...rests} >
             제출하기
         </Button>
     )
--- src/app/thirdparty/validation/zod/no-server-action/page.tsx
+++ src/app/thirdparty/validation/zod/no-server-action/page.tsx
@@ -2,18 +2,20 @@
 import { AntRadio } from '@/app/_ant/Radio';
 import { LoadingButton } from '@/app/_component/LoadingButton';
 import { Flex, Input, } from 'antd';
-import { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod';
 
+const phoneRegExp = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
 const Man = z.object({
-    name: z.string(),
-    height: z.number(),
+    name: z.string().trim().min(1),
+    height: z.number().min(1).max(200),
     age: z.number().min(18, { message: 'too young to play' }).max(40, { message: 'too old to play' }),
-    phoneNum: z.string(),
+    phoneNum: z.string().trim().min(1),
     email: z.string().trim().email().optional(),
     // homePhoneNum: z.string().optional(),
-    isCompletedMilitaryService: z.string(),
+    isCompletedMilitaryService: z.string().optional(),
     // isCompletedMilitaryService: z.string().transform((val) => val === 'true' || val === 'false')
 });
 
@@ -23,8 +25,8 @@ type Man = z.infer<typeof Man>;
 
 type zodSafeParseType = {
     success: boolean;
-    error?: any;
-    data?: any;
+    error?: any[];
+    data?: any[];
 }
 
 type RadioData = {
@@ -33,20 +35,81 @@ type RadioData = {
 }
 
 
+
 export default function ZodNoServerAction() {
-    const [result, setResult] = useState<zodSafeParseType>();
+    const [result, setResult] = useState<any>();
+    const [errorMessage, setErrorMessage] = useState<any>({});
     const [completedMilitaryServiceRadioData, setCompletedMilitaryServiceRadioData] = useState<RadioData[]>([{ key: true, value: 'true' }, { key: false, value: 'false' }]);
+    const [formData, setFormData] = useState<Man>({
+        name: '',
+        height: 0,
+        age: 0,
+        phoneNum: '',
+        email: undefined,
+        isCompletedMilitaryService: undefined
+    })
+    const [disabled, setDisabled] = useState<boolean>(true);
     function check(formData: FormData) {
         const name = formData.get("name");
         const height = Number(formData.get("height"));
         const age = Number(formData.get("age"));
         const phoneNum = formData.get("phoneNum");
-        const eMail = formData.get('eMail');
+        const email = formData.get('email');
         let isCompletedMilitaryService = formData.get("isCompletedMilitaryService");
-        let r = Man.safeParse({ name, height, age, phoneNum, eMail, isCompletedMilitaryService });
-        console.log(r);
+
+        let formObject = { name, height, age, phoneNum }
+        if (email) {
+            formObject = Object.assign({}, formObject, { email });
+        }
+        if (isCompletedMilitaryService) {
+            formObject = Object.assign({}, formObject, { isCompletedMilitaryService: 'true' });
+        }
+        let r = Man.safeParse(formObject);
+
+        console.log({ name, height, age, phoneNum, email, isCompletedMilitaryService })
         setResult(r);
     }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        if (result && result.error) {
+
+            let messageObject: any = {};
+            result.error.issues.forEach((val: any) => {
+                messageObject[val.path[0]] = val;
+            })
+            console.log(result.error.issues, messageObject);
+            setErrorMessage((prev: any) => {
+                return { ...prev, ...messageObject }
+            });
+        } else {
+            setErrorMessage({});
+        }
+    }, [result])
+
+    // useEffect(() => {
+    //     console.log(errorMessage)
+    // }, [errorMessage])
+
+    useEffect(() => {
+        // console.log({ formData })
+        const isDisabled = Object.entries(formData).some(([key, value]) => {
+            console.log({ key, value });
+            if (key === 'email' || key === 'isCompletedMilitaryService') {
+                // console.log("옵션누구임?", key)
+                return false;
+            }
+            return value === '' || value === 0;
+        });
+        setDisabled(isDisabled);
+    }, [formData]);
 
 
     return (
@@ -56,14 +119,20 @@ export default function ZodNoServerAction() {
             <p>이메일은 선택입니다</p>
             <form action={check}>
                 <Flex gap='middle' vertical style={{ width: '400px' }}>
-                    <Input name="name" placeholder="id를 입력하세요" />
-                    <Input type='number' name="height" placeholder="키를 입력하세요" />
-                    <Input type='number' name="age" placeholder="나이를 입력하세요" />
-                    <Input type='number' name="phoneNum" placeholder="핸드폰 번호를 입력하세요" />
-                    <Input type='text' name="eMail" placeholder="이메일을 입력하세요" />
+                    <Input name="name" value={formData.name} onChange={handleChange} placeholder="name을 입력하세요" status={errorMessage && errorMessage['name'] ? 'error' : ''} />
+                    {errorMessage && errorMessage['name'] && (<div>{errorMessage['name'].message}</div>)}
+                    <Input type='number' value={formData.height} onChange={handleChange} name="height" placeholder="키를 입력하세요" status={errorMessage && errorMessage['height'] ? 'error' : ''} />
+                    {errorMessage && errorMessage['height'] && (<div>{errorMessage['height'].message}</div>)}
+                    <Input type='number' value={formData.age} onChange={handleChange} name="age" placeholder="나이를 입력하세요" status={errorMessage && errorMessage['age'] ? 'error' : ''} />
+                    {errorMessage && errorMessage['age'] && (<div>{errorMessage['age'].message}</div>)}
+                    <Input type='number' value={formData.phoneNum} onChange={handleChange} name="phoneNum" placeholder="핸드폰 번호를 입력하세요" status={errorMessage && errorMessage['phoneNum'] ? 'error' : ''} />
+                    {errorMessage && errorMessage['phoneNum'] && (<div>{errorMessage['phoneNum'].message}</div>)}
+                    <Input type='text' value={formData.email} onChange={handleChange} name="email" placeholder="이메일을 입력하세요" status={errorMessage && errorMessage['email'] ? 'error' : ''} />
+                    {errorMessage && errorMessage['email'] && (<div>{errorMessage['email'].message}</div>)}
                     {/* <Input name="isCompletedMilitaryService" placeholder="군필여부를적어주세요 true or false" /> */}
                     <AntRadio name='isCompletedMilitaryService' data={completedMilitaryServiceRadioData} />
-                    <LoadingButton htmlType={'submit'} times={1000} />
+                    {errorMessage && errorMessage['isCompletedMilitaryService'] && (<div>{errorMessage['isCompletedMilitaryService'].message}</div>)}
+                    <LoadingButton htmlType={'submit'} onCompleted={result ? true : false} disabled={disabled} />
                 </Flex>
             </form>
             <h2>결과</h2>
